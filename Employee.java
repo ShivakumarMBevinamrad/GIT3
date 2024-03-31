f3be9d6521036eba554566f7918a1f20e40649fd
Shivakumar <shivakumarbevinamarad62@gmail.com>
2024-03-31 15:15:08 +0400

    Employee Personal Information

---
+++ Employee.java
@@ -0,0 +1,26 @@
+class Employee {
+    protected String name;
+    protected int employeeId;
+    protected double salary;
+
+    public Employee(String name, int employeeId, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    public void displayEmployeeDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Salary: $" + salary);
+    }
+
+    public static void main(String[] args) {
+        // Creating Employee object
+        Employee employee = new Employee("John Doe", 1001, 50000);
+        System.out.println("Employee Details:");
+        employee.displayEmployeeDetails();
+
+        System.out.println();
+    }
+}
